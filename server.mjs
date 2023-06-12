9ea3a1e3cd161e1a590be9cc4bd51aae6beb4b5e
devStallyons <stallyons.tester2@gmail.com>
2023-06-12 13:57:52 -0200

    cors error

--- server.mjs
+++ server.mjs
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://localhost:3000', "*"],
+    origin: ['https://questionare-admin.web.app', 'https://questionare-quest.web.app', "*"],
     credentials: true
 }));
 
